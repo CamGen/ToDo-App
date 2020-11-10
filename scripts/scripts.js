e563bd900ee8cb50cafa0b274b0be633124d57fb
CamGen <ccdorning@gmail.com>
2020-11-10 06:28:53 +0200

    Adding js, needs work

---
+++ scripts/scripts.js
@@ -0,0 +1,7 @@
+/* Adding a function for checking an item */
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
