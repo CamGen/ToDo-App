49c2c5bcff0d07319ddabf8006260f919290e2d6
CamGen <ccdorning@gmail.com>
2020-11-12 19:15:59 +0200

    fixing formatting, working on js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,4 @@
-/* Adding a function for ticking off an item */
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-    if (ev.target.tagName === 'LI') {
-        ev.target.classList.toggle('checked');
-    }
-}, false);
-
 /* Create remove symbol ("cross") button */
-function removeSymbol(){
     var myNodelist = document.getElementsByTagName("LI");
     var i;
     for (i = 0; i < myNodelist.length; i++) {
@@ -17,9 +8,6 @@ function removeSymbol(){
         span.appendChild(txt);
         myNodelist[i].appendChild(span);
     }
-}
-
-removeSymbol();
 
 /* Click on the remove button */
 var close = document.getElementsByClassName("remove");
@@ -29,14 +17,21 @@ for (i = 0; i < close.length; i++) {
     var div = this.parentElement;
     div.style.display = "none";
   }
-  removeSymbol();
 }
+
+/* Adding a function for ticking off an item */
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
 
 /* Adding a new item to the list */
 function newItem() {
-    let listItem = document.createElement("li");
-    let inputItem = document.getElementById("inputItem").value;
-    let textInput = document.createTextNode(inputItem);
+    var listItem = document.createElement("li");
+    var inputItem = document.getElementById("inputItem").value;
+    var textInput = document.createTextNode(inputItem);
     listItem.appendChild(textInput);
     if (inputItem === "") {
         alert("Please enter an item description!");
@@ -49,7 +44,7 @@ function newItem() {
     var txt = document.createTextNode("\u00D7");
     span.className = "remove";
     span.appendChild(txt);
-    li.appendChild(span);
+    liatItem.appendChild(span);
 
     for (i = 0; i < close.length; i++) {
         close[i].onclick = function() {
